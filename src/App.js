6f5f5c5b8d0310446a86ad1e3c26d0bc2a07b420
a2nurag <anurag2sarkar@gmail.com>
2022-02-24 01:09:09 +0530

    made the improvements

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
-import "./App.css";
-import { Box } from "@mui/system";
-import { useState, useEffect } from "react";
-import MatrixController from "./MatrixController";
-import Matrices from "./Matrices";
+import './App.css'
+import { Box } from '@mui/system'
+import { Button } from '@mui/material'
+import { useState, useEffect } from 'react'
+import MatrixController from './MatrixController'
+import Matrices from './Matrices'
+import CloseIcon from '@mui/icons-material/Close'
 
 const App = () => {
   //red
-  const [red1_1, setred1_1] = useState(0);
-  const [red1_2, setred1_2] = useState(0);
-  const [red1_3, setred1_3] = useState(0);
-  const [red1_4, setred1_4] = useState(0);
+  const [red1_1, setred1_1] = useState(0)
+  const [red1_2, setred1_2] = useState(0)
+  const [red1_3, setred1_3] = useState(0)
+  const [red1_4, setred1_4] = useState(0)
 
-  const [red2_1, setred2_1] = useState(0);
-  const [red2_2, setred2_2] = useState(0);
-  const [red2_3, setred2_3] = useState(0);
-  const [red2_4, setred2_4] = useState(0);
+  const [red2_1, setred2_1] = useState(0)
+  const [red2_2, setred2_2] = useState(0)
+  const [red2_3, setred2_3] = useState(0)
+  const [red2_4, setred2_4] = useState(0)
   //blue
-  const [blue1_1, setblue1_1] = useState(0);
-  const [blue1_2, setblue1_2] = useState(0);
-  const [blue1_3, setblue1_3] = useState(0);
-  const [blue1_4, setblue1_4] = useState(0);
+  const [blue1_1, setblue1_1] = useState(0)
+  const [blue1_2, setblue1_2] = useState(0)
+  const [blue1_3, setblue1_3] = useState(0)
+  const [blue1_4, setblue1_4] = useState(0)
 
-  const [blue2_1, setblue2_1] = useState(0);
-  const [blue2_2, setblue2_2] = useState(0);
-  const [blue2_3, setblue2_3] = useState(0);
-  const [blue2_4, setblue2_4] = useState(0);
+  const [blue2_1, setblue2_1] = useState(0)
+  const [blue2_2, setblue2_2] = useState(0)
+  const [blue2_3, setblue2_3] = useState(0)
+  const [blue2_4, setblue2_4] = useState(0)
   //green
-  const [green1_1, setgreen1_1] = useState(0);
-  const [green1_2, setgreen1_2] = useState(0);
-  const [green1_3, setgreen1_3] = useState(0);
-  const [green1_4, setgreen1_4] = useState(0);
+  const [green1_1, setgreen1_1] = useState(0)
+  const [green1_2, setgreen1_2] = useState(0)
+  const [green1_3, setgreen1_3] = useState(0)
+  const [green1_4, setgreen1_4] = useState(0)
 
-  const [green2_1, setgreen2_1] = useState(0);
-  const [green2_2, setgreen2_2] = useState(0);
-  const [green2_3, setgreen2_3] = useState(0);
-  const [green2_4, setgreen2_4] = useState(0);
+  const [green2_1, setgreen2_1] = useState(0)
+  const [green2_2, setgreen2_2] = useState(0)
+  const [green2_3, setgreen2_3] = useState(0)
+  const [green2_4, setgreen2_4] = useState(0)
   //yellow
-  const [yellow1_1, setyellow1_1] = useState(0);
-  const [yellow1_2, setyellow1_2] = useState(0);
-  const [yellow1_3, setyellow1_3] = useState(0);
-  const [yellow1_4, setyellow1_4] = useState(0);
+  const [yellow1_1, setyellow1_1] = useState(0)
+  const [yellow1_2, setyellow1_2] = useState(0)
+  const [yellow1_3, setyellow1_3] = useState(0)
+  const [yellow1_4, setyellow1_4] = useState(0)
 
-  const [yellow2_1, setyellow2_1] = useState(0);
-  const [yellow2_2, setyellow2_2] = useState(0);
-  const [yellow2_3, setyellow2_3] = useState(0);
-  const [yellow2_4, setyellow2_4] = useState(0);
+  const [yellow2_1, setyellow2_1] = useState(0)
+  const [yellow2_2, setyellow2_2] = useState(0)
+  const [yellow2_3, setyellow2_3] = useState(0)
+  const [yellow2_4, setyellow2_4] = useState(0)
 
-  const [maxZ, setMaxZ] = useState(9);
-  const red = "#F05B5B",
-    blue = "#51B8F2",
-    green = "#6FCF97",
-    yellow = "#F2C94C";
+  const [maxZ, setMaxZ] = useState(9)
+  const red = '#F05B5B',
+    blue = '#51B8F2',
+    green = '#6FCF97',
+    yellow = '#F2C94C'
+
+  const indicators = {
+    position: 'absolute',
+    zIndex: '9999',
+    width: '35px',
+    height: '35px',
+    bgcolor: '#EEEEEE',
+    display: 'grid',
+    placeItems: 'center',
+    borderRadius: '50px',
+    fontWeight: 'bold',
+    pointerEvents: 'none',
+  }
 
   const matrices = [
     {
-      color: "red-1",
-      id: "matrix-red-1-1",
-      colorId: "#F05B5B",
+      color: 'red-1',
+      id: 'matrix-red-1-1',
+      colorId: '#F05B5B',
       top: 0,
       left: 0,
       state_1: red1_1,
@@ -70,8 +85,8 @@ const App = () => {
       z: 1,
     },
     {
-      color: "red-2",
-      id: "matrix-red-2-1",
+      color: 'red-2',
+      id: 'matrix-red-2-1',
       colorId: red,
       top: 375,
       left: 750,
@@ -86,8 +101,8 @@ const App = () => {
       z: 2,
     },
     {
-      color: "blue-1",
-      id: "matrix-blue-1-1",
+      color: 'blue-1',
+      id: 'matrix-blue-1-1',
       colorId: blue,
       top: 0,
       left: 375,
@@ -102,8 +117,8 @@ const App = () => {
       z: 3,
     },
     {
-      color: "blue-2",
-      id: "matrix-blue-2-1",
+      color: 'blue-2',
+      id: 'matrix-blue-2-1',
       colorId: blue,
       top: 750,
       left: 0,
@@ -118,8 +133,8 @@ const App = () => {
       z: 4,
     },
     {
-      color: "green-1",
-      id: "matrix-green-1-1",
+      color: 'green-1',
+      id: 'matrix-green-1-1',
       colorId: green,
       top: 0,
       left: 750,
@@ -134,8 +149,8 @@ const App = () => {
       z: 5,
     },
     {
-      color: "green-2",
-      id: "matrix-green-2-1",
+      color: 'green-2',
+      id: 'matrix-green-2-1',
       colorId: green,
       top: 750,
       left: 375,
@@ -150,8 +165,8 @@ const App = () => {
       z: 6,
     },
     {
-      color: "yellow-1",
-      id: "matrix-yellow-1-1",
+      color: 'yellow-1',
+      id: 'matrix-yellow-1-1',
       colorId: yellow,
       top: 375,
       left: 0,
@@ -166,8 +181,8 @@ const App = () => {
       z: 3,
     },
     {
-      color: "yellow-2",
-      id: "matrix-yellow-2-1",
+      color: 'yellow-2',
+      id: 'matrix-yellow-2-1',
       colorId: yellow,
       top: 750,
       left: 750,
@@ -181,45 +196,69 @@ const App = () => {
       setState_4: setyellow2_4,
       z: 4,
     },
-  ];
+  ]
 
   useEffect(() => {
     window.onbeforeunload = (e) => {
-      return "Data will be lost if you leave the page, are you sure?";
-    };
-  }, []);
+      return 'Data will be lost if you leave the page, are you sure?'
+    }
+  }, [])
 
   return (
     <>
-      <Box sx={{ display: "flex", alignItems: "center", gap: "4em" }}>
-        {}
-        <Box className="matrix-header">
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '4em' }}>
+        <Box className='matrix-header'>
           <h1>MATRIX 2022</h1>
         </Box>
         <Box
           sx={{
-            display: "grid",
-            gridTemplateRows: "repeat(12, 75px)",
-            gridTemplateColumns: "repeat(12,75px)",
-            position: "relative",
-            width: "max-content",
-            border: "1px solid #999999",
-            overflow: "hidden",
+            display: 'grid',
+            gridTemplateRows: 'repeat(12, 75px)',
+            gridTemplateColumns: 'repeat(12,75px)',
+            position: 'relative',
+            width: 'max-content',
+            border: '1px solid #999999',
           }}
         >
+          <Box sx={{ ...indicators, left: 'calc(50% - 17px)', top: '-17px' }}>
+            N
+          </Box>
+
+          <Box
+            sx={{ ...indicators, left: 'calc(50% - 17px)', bottom: '-17px' }}
+          >
+            S
+          </Box>
+
+          <Box sx={{ ...indicators, top: 'calc(50% - 17px)', right: '-17px' }}>
+            E
+          </Box>
+
+          <Box sx={{ ...indicators, top: 'calc(50% - 17px)', left: '-17px' }}>
+            W
+          </Box>
+
+          <Box sx={{ ...indicators, left: '-17px', top: '-17px' }}>NW</Box>
+
+          <Box sx={{ ...indicators, left: '-17px', bottom: '-17px' }}>SW</Box>
+
+          <Box sx={{ ...indicators, right: '-17px', top: '-17px' }}>NE</Box>
+
+          <Box sx={{ ...indicators, right: '-17px', bottom: '-17px' }}>SE</Box>
+
           {new Array(144).fill(null).map((item, index) => (
-            <Box sx={{ border: "1px solid #999999" }} key={index} />
+            <Box sx={{ border: '1px solid #999999' }} key={index} />
           ))}
           <Box
             sx={{
-              border: "1px solid #999999",
-              width: "1100px",
-              bgcolor: "#999999",
-              height: "1px",
-              transform: "rotate(-33.5deg)",
-              position: "absolute",
-              top: "450px",
-              left: "-100px",
+              border: '1px solid #999999',
+              width: '1100px',
+              bgcolor: '#999999',
+              height: '1px',
+              transform: 'rotate(-33.5deg)',
+              position: 'absolute',
+              top: '450px',
+              left: '-100px',
             }}
           />
 
@@ -251,7 +290,13 @@ const App = () => {
           )}
         </Box>
 
-        <Box sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            gap: '2rem',
+          }}
+        >
           {matrices.map(
             ({
               color,
@@ -286,8 +331,132 @@ const App = () => {
           )}
         </Box>
       </Box>
-    </>
-  );
-};
 
-export default App;
+      <Box
+        sx={{
+          marginTop: '5rem',
+          display: 'flex',
+          gap: '1rem',
+        }}
+      >
+        <Button
+          variant='contained'
+          sx={{ bgcolor: `${red}`, color: 'black' }}
+          onClick={() => {
+            let ans = window.confirm('Do u want to end game for Red?')
+            if (ans === false) return
+            const red1 = document.querySelector('#matrix-red-1-1')
+            const red2 = document.querySelector('#matrix-red-2-1')
+            const red1_c = document.querySelector('#matrix-red-1-1-controller')
+            const red2_c = document.querySelector('#matrix-red-2-1-controller')
+
+            red1.style.opacity = '0.4'
+            red1.style.zIndex = '0'
+            red1_c.style.opacity = '0.4'
+            red1_c.style.pointerEvents = 'none'
+
+            red2.style.opacity = '0.4'
+            red2.style.zIndex = '0'
+            red2_c.style.opacity = '0.4'
+            red2_c.style.pointerEvents = 'none'
+          }}
+        >
+          <CloseIcon />
+          End Game for Red
+        </Button>
+
+        <Button
+          variant='contained'
+          sx={{ bgcolor: `${blue}`, color: 'black' }}
+          onClick={() => {
+            let ans = window.confirm('Do u want to end game for blue?')
+            if (ans === false) return
+            const blue1 = document.querySelector('#matrix-blue-1-1')
+            const blue2 = document.querySelector('#matrix-blue-2-1')
+            const blue1_c = document.querySelector(
+              '#matrix-blue-1-1-controller'
+            )
+            const blue2_c = document.querySelector(
+              '#matrix-blue-2-1-controller'
+            )
+
+            blue1.style.opacity = '0.4'
+            blue1.style.zIndex = '0'
+            blue1_c.style.opacity = '0.4'
+            blue1_c.style.pointerEvents = 'none'
+
+            blue2.style.opacity = '0.4'
+            blue2.style.zIndex = '0'
+            blue2_c.style.opacity = '0.4'
+            blue2_c.style.pointerEvents = 'none'
+          }}
+        >
+          <CloseIcon />
+          End Game for Blue
+        </Button>
+
+        <Button
+          variant='contained'
+          sx={{ bgcolor: `${green}`, color: 'black' }}
+          onClick={() => {
+            let ans = window.confirm('Do u want to end game for green?')
+            if (ans === false) return
+            const green1 = document.querySelector('#matrix-green-1-1')
+            const green2 = document.querySelector('#matrix-green-2-1')
+            const green1_c = document.querySelector(
+              '#matrix-green-1-1-controller'
+            )
+            const green2_c = document.querySelector(
+              '#matrix-green-2-1-controller'
+            )
+
+            green1.style.opacity = '0.4'
+            green1.style.zIndex = '0'
+            green1_c.style.opacity = '0.4'
+            green1_c.style.pointerEvents = 'none'
+
+            green2.style.opacity = '0.4'
+            green2.style.zIndex = '0'
+            green2_c.style.opacity = '0.4'
+            green2_c.style.pointerEvents = 'none'
+          }}
+        >
+          <CloseIcon />
+          End Game for Green
+        </Button>
+
+        <Button
+          variant='contained'
+          sx={{ bgcolor: `${yellow}`, color: 'black' }}
+          onClick={() => {
+            let ans = window.confirm('Do u want to end game for yellow?')
+            if (ans === false) return
+            const yellow1 = document.querySelector('#matrix-yellow-1-1')
+            const yellow2 = document.querySelector('#matrix-yellow-2-1')
+            const yellow1_c = document.querySelector(
+              '#matrix-yellow-1-1-controller'
+            )
+            const yellow2_c = document.querySelector(
+              '#matrix-yellow-2-1-controller'
+            )
+
+            yellow1.style.opacity = '0.4'
+            yellow1.style.zIndex = '0'
+            yellow1_c.style.opacity = '0.4'
+            yellow1_c.style.pointerEvents = 'none'
+
+            yellow2.style.opacity = '0.4'
+            yellow2.style.zIndex = '0'
+            yellow2_c.style.opacity = '0.4'
+            yellow2_c.style.pointerEvents = 'none'
+          }}
+        >
+          <CloseIcon />
+          End Game for Yellow
+        </Button>
+      </Box>
+    </>
+  )
+}
+
+export default App
--- src/MatrixController.js
+++ src/MatrixController.js
@@ -1,11 +1,15 @@
-import "./App.css";
-import { Button } from "@mui/material";
-import { Box } from "@mui/system";
+import './App.css'
+import { Button } from '@mui/material'
+import { Box } from '@mui/system'
 
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import NorthIcon from '@mui/icons-material/North'
+import SouthIcon from '@mui/icons-material/South'
+import EastIcon from '@mui/icons-material/East'
+import WestIcon from '@mui/icons-material/West'
+import NorthEastIcon from '@mui/icons-material/NorthEast'
+import NorthWestIcon from '@mui/icons-material/NorthWest'
+import SouthWestIcon from '@mui/icons-material/SouthWest'
+import SouthEastIcon from '@mui/icons-material/SouthEast'
 
 export default function MatrixController({
   color,
@@ -24,130 +28,195 @@ export default function MatrixController({
 }) {
   //color of format red-1
   const inputCss = {
-    width: "50px",
-    height: "50px",
-    backgroundColor: "transparent",
-    border: "none",
-    fontSize: "20px",
-    fontWeight: "bold",
-    outline: "none",
-    color: "#000",
-  };
+    width: '50px',
+    height: '50px',
+    backgroundColor: 'transparent',
+    border: 'none',
+    fontSize: '20px',
+    fontWeight: 'bold',
+    outline: 'none',
+    color: '#000',
+  }
 
   const onChangeHandler = (state, setState, id) => {
-    let currentState = document.querySelector(id).value;
-    setState(currentState);
-  };
+    let currentState = document.querySelector(id).value
+    setState(currentState)
+  }
 
   const onClickHandler = (id, top, left) => {
-    let matrix = document.querySelector(`#${id}`);
-    let currentTop = parseInt(getComputedStyle(matrix).getPropertyValue("top"));
+    let matrix = document.querySelector(`#${id}`)
+    let currentTop = parseInt(getComputedStyle(matrix).getPropertyValue('top'))
     let currentLeft = parseInt(
-      getComputedStyle(matrix).getPropertyValue("left")
-    );
+      getComputedStyle(matrix).getPropertyValue('left')
+    )
 
-    let newLeft = currentLeft + left;
-    let newTop = currentTop + top;
+    let newLeft = currentLeft + left
+    let newTop = currentTop + top
 
-    if (newTop >= 0 && newTop <= 750) matrix.style.top = `${newTop}px`;
-    if (newLeft >= 0 && newLeft <= 750) matrix.style.left = `${newLeft}px`;
-  };
+    if (newTop >= 0 && newTop <= 750) matrix.style.top = `${newTop}px`
+    if (newLeft >= 0 && newLeft <= 750) matrix.style.left = `${newLeft}px`
+  }
 
   return (
     <Box
       sx={{
-        display: "grid",
-        gridTemplateColumns: "50px 50px 50px 50px",
-        gridTemplateRows: "50px 50px 50px 50px 50px",
-        gap: "2px",
+        display: 'grid',
+        gridTemplateColumns: '50px 50px 50px 50px',
+        gridTemplateRows: '50px 50px 50px 50px 50px',
+        gap: '2px',
       }}
       onClick={() => {
-        let m = document.querySelector(`#${idOfMatrix}`);
-        m.style.zIndex = z;
-        setZ(z + 1);
+        let m = document.querySelector(`#${idOfMatrix}`)
+        m.style.zIndex = z
+        setZ(z + 1)
       }}
+      id={`${idOfMatrix}-controller`}
     >
       {/* Up */}
       <Button
         sx={{
-          gridRow: "1 / 2",
-          gridColumn: "2 / 4",
-          padding: "0",
-          width: "100px",
-          minWidth: "100px",
-          height: "50px",
+          gridRow: '1 / 2',
+          gridColumn: '2 / 4',
+          padding: '0',
+          width: '100px',
+          minWidth: '100px',
+          height: '50px',
           color: `${colorId}`,
         }}
         onClick={() => onClickHandler(idOfMatrix, -75, 0)}
       >
-        <ArrowUpwardIcon />
+        <NorthIcon />
       </Button>
 
       {/* Down */}
       <Button
         sx={{
-          gridRow: "4 / 5",
-          gridColumn: "2 / 4",
-          padding: "0",
-          width: "50px",
-          minWidth: "100px",
-          height: "50px",
+          gridRow: '4 / 5',
+          gridColumn: '2 / 4',
+          padding: '0',
+          width: '50px',
+          minWidth: '100px',
+          height: '50px',
           color: `${colorId}`,
         }}
         onClick={() => onClickHandler(idOfMatrix, 75, 0)}
       >
-        <ArrowDownwardIcon />
+        <SouthIcon />
       </Button>
 
       {/* Left */}
       <Button
         sx={{
-          gridRow: "2 / 4",
-          gridColumn: "1 / 2",
-          padding: "0",
-          width: "50px",
-          minWidth: "50px",
-          height: "100px",
+          gridRow: '2 / 4',
+          gridColumn: '1 / 2',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '100px',
           color: `${colorId}`,
         }}
         onClick={() => onClickHandler(idOfMatrix, 0, -75)}
       >
-        <ArrowBackIcon />
+        <WestIcon />
+      </Button>
+
+      {/* Top Left */}
+      <Button
+        sx={{
+          gridRow: '1 / 2',
+          gridColumn: '1 / 2',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '50px',
+          color: `${colorId}`,
+        }}
+        onClick={() => onClickHandler(idOfMatrix, -75, -75)}
+      >
+        <NorthWestIcon />
+      </Button>
+
+      {/* Top Right */}
+      <Button
+        sx={{
+          gridRow: '1 / 2',
+          gridColumn: '4 / 5',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '50px',
+          color: `${colorId}`,
+        }}
+        onClick={() => onClickHandler(idOfMatrix, -75, 75)}
+      >
+        <NorthEastIcon />
+      </Button>
+
+      {/* Bottom Left */}
+      <Button
+        sx={{
+          gridRow: '4 / 5',
+          gridColumn: '1 / 2',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '50px',
+          color: `${colorId}`,
+        }}
+        onClick={() => onClickHandler(idOfMatrix, 75, -75)}
+      >
+        <SouthWestIcon />
+      </Button>
+
+      {/* Bottom Right */}
+      <Button
+        sx={{
+          gridRow: '4 / 5',
+          gridColumn: '4 / 5',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '50px',
+          color: `${colorId}`,
+        }}
+        onClick={() => onClickHandler(idOfMatrix, 75, 75)}
+      >
+        <SouthEastIcon />
       </Button>
 
       {/* Right */}
       <Button
         sx={{
-          gridRow: "2 / 4",
-          gridColumn: "4 / 5",
-          padding: "0",
-          width: "50px",
-          minWidth: "50px",
-          height: "100px",
+          gridRow: '2 / 4',
+          gridColumn: '4 / 5',
+          padding: '0',
+          width: '50px',
+          minWidth: '50px',
+          height: '100px',
           color: `${colorId}`,
         }}
         onClick={() => onClickHandler(idOfMatrix, 0, 75)}
       >
-        <ArrowForwardIcon />
+        <EastIcon />
       </Button>
 
       {/* Matrix */}
       {/* 0,0 */}
       <Box
         sx={{
-          gridRow: "2 / 3",
-          gridColumn: "2 / 3",
+          gridRow: '2 / 3',
+          gridColumn: '2 / 3',
           bgcolor: `${colorId}`,
-          display: "block",
+          display: 'block',
         }}
       >
         <input
-          type="number"
+          type='number'
           id={`${color}-1`}
           style={inputCss}
           defaultValue={state_1}
           onChange={() => {
-            onChangeHandler(state_1, setState_1, `#${color}-1`);
+            onChangeHandler(state_1, setState_1, `#${color}-1`)
           }}
         />
       </Box>
@@ -155,19 +224,19 @@ export default function MatrixController({
       {/* 0,1 */}
       <Box
         sx={{
-          gridRow: "2 / 3",
-          gridColumn: "3 / 4",
+          gridRow: '2 / 3',
+          gridColumn: '3 / 4',
           bgcolor: `${colorId}`,
-          display: "block",
+          display: 'block',
         }}
       >
         <input
-          type="number"
+          type='number'
           id={`${color}-2`}
           style={inputCss}
           defaultValue={state_2}
           onChange={() => {
-            onChangeHandler(state_2, setState_2, `#${color}-2`);
+            onChangeHandler(state_2, setState_2, `#${color}-2`)
           }}
         />
       </Box>
@@ -175,18 +244,18 @@ export default function MatrixController({
       {/* 1,0 */}
       <Box
         sx={{
-          gridRow: "3 / 4",
-          gridColumn: "2 / 3",
+          gridRow: '3 / 4',
+          gridColumn: '2 / 3',
           bgcolor: `${colorId}`,
-          display: "block",
+          display: 'block',
         }}
       >
         <input
-          type="number"
+          type='number'
           id={`${color}-3`}
           style={inputCss}
           onChange={() => {
-            onChangeHandler(state_3, setState_3, `#${color}-3`);
+            onChangeHandler(state_3, setState_3, `#${color}-3`)
           }}
           defaultValue={state_3}
         />
@@ -195,32 +264,37 @@ export default function MatrixController({
       {/* 1,1 */}
       <Box
         sx={{
-          gridRow: "3 / 4",
-          gridColumn: "3 / 4",
+          gridRow: '3 / 4',
+          gridColumn: '3 / 4',
           bgcolor: `${colorId}`,
-          display: "block",
+          display: 'block',
         }}
       >
         <input
-          type="number"
+          type='number'
           id={`${color}-4`}
           style={inputCss}
           defaultValue={state_4}
           onChange={() => {
-            onChangeHandler(state_4, setState_4, `#${color}-4`);
+            onChangeHandler(state_4, setState_4, `#${color}-4`)
           }}
         />
       </Box>
-      <h3
+      <input
         style={{
-          gridRow: "5 / 6",
-          gridColumn: "1 / 5",
-          margin: "auto",
+          gridRow: '5 / 6',
+          gridColumn: '1 / 6',
+          margin: 'auto',
           color: `${colorId}`,
+          backgroundColor: '#363636',
+          fontSize: '1.2rem',
+          fontWeight: 'bold',
+          maxWidth: '150px',
+          border: 'none',
+          textAlign: 'center',
         }}
-      >
-        TEAM {color.toUpperCase()}
-      </h3>
+        defaultValue={`TEAM ${color.toUpperCase()}`}
+      />
     </Box>
-  );
+  )
 }
